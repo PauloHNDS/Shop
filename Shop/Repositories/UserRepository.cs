c6e467ff8fe56ef51caa9d1b60ad919364e1e15f
Paulo Henrique Nascimento Dos Santos <69486078+PauloHNDS@users.noreply.github.com>
2022-04-01 14:02:59 -0300

    Add files via upload

---
+++ Shop/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Shop.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shop.Repositories
+{
+    public static class UserRepository
+    {
+        public static User Get(string username, string password) 
+        {
+            var users = new List<User>();
+            users.Add(new User {Id = 1, UserName= "Paulo" , Password = "batman" , Role="employee" });
+            users.Add(new User { Id = 2, UserName = "Sandro", Password="robin", Role="employee"});
+            return users.Where(x => x.UserName.ToLower() == username.ToLower() && x.Password.ToLower() == password.ToLower() ).FirstOrDefault();
+        }
+    }
+}
